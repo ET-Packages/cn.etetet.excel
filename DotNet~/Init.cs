4d087e79002067c50a2c93925be451b651f8d361
tanghai <egametang@gmail.com>
2024-06-10 16:24:46 +0800

    init

---
+++ DotNet~/Init.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ET
+{
+    internal static class Init
+    {
+        private static int Main(string[] args)
+        {
+            try
+            {
+                ExcelExporter.Export();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.WriteLine("excelexporter ok!");
+            return 1;
+        }
+    }
+}
